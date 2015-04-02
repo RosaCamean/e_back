8be79dca0bdf04a3b0a53926e0ec7fa9a79bce2b
Rosa <rosa.camean@gmail.com>
2015-04-02 14:26:30 +0200

    Funcionalidad OpenGL
    Añadida funcionalidad OpenGL

--- app/src/main/java/proyects/e_back/MainActivity.java
+++ app/src/main/java/proyects/e_back/MainActivity.java
@@ -1,5 +1,6 @@
 package proyects.e_back;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +9,13 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private GLSurfaceView mGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
     }
 
 
---
+++ app/src/main/java/proyects/e_back/MyGLSurfaceView.java
@@ -0,0 +1,26 @@
+package proyects.e_back;
+
+
+import android.opengl.GLSurfaceView;
+import android.content.Context;
+
+public class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+        // Create an OpenGL ES 2.0 context.
+        setEGLContextClientVersion(2);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new MyGLRenderer();
+        setRenderer(mRenderer);
+
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+
+}
